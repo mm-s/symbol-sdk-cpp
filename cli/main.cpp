bab477e656c24ea006f9266264bf51aa5a077eab
Marcos Mayorga <marcos.mayorga@nem.software>
2021-06-10 19:49:58 +0100

    style: tabs spaces

--- cli/main.cpp
+++ cli/main.cpp
@@ -7,16 +7,16 @@ int main(int argc, char** argv) {
 	using namespace std;
 	using Cli = symbol::Hmi;
 
-    ostringstream os;
-    for (int i=1; i< argc; ++i) {
-        os << argv[i] << ' ';
-    }
-    string cmdline=os.str();
+	ostringstream os;
+	for (int i=1; i< argc; ++i) {
+		os << argv[i] << ' ';
+	}
+	string cmdline=os.str();
 
-    Cli::Section::init(cout, cerr, "0.0.1");
-    Cli cli;
-    cli.init(argv[0], "Symbol wallet.");
+	Cli::Section::init(cout, cerr, "0.0.1");
+	Cli cli;
+	cli.init(argv[0], "Symbol wallet.");
 	//TODO: interactive shell
-    return cli.exec(cmdline)?0:1;
+	return cli.exec(cmdline)?0:1;
 }
 
