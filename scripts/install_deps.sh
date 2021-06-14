2b007ebe43760800897356e464ddc02259f141a2
Marcos Mayorga <marcos.mayorga@nem.software>
2021-06-14 19:41:02 +0100

    build: added automation script for building and installing dependencies

---
+++ scripts/install_deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+debs="g++ cmake git libboost1.74-all-dev"
+
+apt install $debs
+
+git clone https://github.com/mm-s/restc-cpp
+cd restc-cpp
+git checkout marc-os
+mkdir _build
+cd _build
+cmake -DRESTC_CPP_USE_CPP20=ON -DRESTC_CPP_PIC=ON ..
+make install
+
+exit 0
+
